b08c692e60935594ee4827e0d190848a37dfd827
altafansari-ds <75256060+altafansari-ds@users.noreply.github.com>
2020-12-17 17:24:31 +0530

    Add files via upload

--- task_1/app.py
+++ task_1/app.py
@@ -1,7 +1,7 @@
 from flask import Flask,render_template,request
 app=Flask(__name__)
 
-@app.route('/')
+@app.route('/',methods=['Get','Post'])
 def home():
     return render_template('layout.html')
 
